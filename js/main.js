787dc036007294288ead3fe56065acc676efa881
lovekeshpal <lovekeshfgr@gmail.com>
2023-09-21 22:45:49 +0530

    added the validation for task description

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 // For task name validation
 let taskName = document.getElementById("taskName"); //to get the task name
 let taskNameError = document.getElementById("taskNameError"); //task name error div
-let p = document.createElement("p"); //to create the paragraph element
-let taskNameInputError = false; // for toggle the error display
+let taskNameErrorParagraph = document.createElement("p"); //to create the paragraph element
+let taskNameInputError = false; // for toggle the error display in task name
+
+let taskDescription = document.getElementById("taskDescription"); //to get the task description
+let taskDescriptionError = document.getElementById("taskDescriptionError"); //task description error div
+let taskDescriptionErrorParagraph = document.createElement("p"); //to create the paragraph element
+let taskDescriptionInputError = false;
 
 // For task description validation
 
@@ -10,9 +15,17 @@ function addTask() {
   if (taskName.value == "") {
     taskNameInputError = true;
     taskName.classList.add("input-error");
-    taskNameError.append(p);
-    p.append("Please enter task name.");
-    p.classList.add("error-font");
+    taskNameError.append(taskNameErrorParagraph);
+    taskNameErrorParagraph.append("Please enter task name.");
+    taskNameErrorParagraph.classList.add("error-font");
+  }
+
+  if (taskDescription.value == "") {
+    taskDescriptionInputError = true;
+    taskDescription.classList.add("input-error");
+    taskDescriptionError.append(taskDescriptionErrorParagraph);
+    taskDescriptionErrorParagraph.append("Please write task description.");
+    taskDescriptionErrorParagraph.classList.add("error-font");
   }
 }
 
@@ -20,6 +33,8 @@ addEventListener("input", (event) => {
   if (taskNameInputError) {
     taskName.classList.remove("input-error");
     taskNameError.remove(p);
+    taskDescriptionError.remove(p);
     taskNameInputError = false;
+    taskDescriptionInputError = false;
   }
 });
